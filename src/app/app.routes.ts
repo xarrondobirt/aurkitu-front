0036d1d561a2b70d3651843276f69a59f1618022
jiocariz-birt <jocariz@birt.eus>
2025-12-23 00:13:22 +0100

    Componente aurkitu-map para seleccionar ubicación y radio alrededor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,14 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.routes').then((m) => m.routes),
-  },  {
+  },
+  {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  /*{
+    path: 'prueba-mapa',
+    loadComponent: () => import('./pages/prueba-mapa/prueba-mapa.page').then( m => m.PruebaMapaPage)
+  },*/
 
 ];
